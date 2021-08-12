d476f69c17a0eb5a676a9c1581750d09655f8667
nestor677 <nestorfabi677@gmail.com>
2021-08-12 10:30:43 -0300

    make it better

--- clinica/settings/productions.py
+++ clinica/settings/productions.py
@@ -9,13 +9,6 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
